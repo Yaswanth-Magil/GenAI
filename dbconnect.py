e949e76b89c4bc868d71b174083b5b325b464728
Yaswanth-Magil <yaswanth.boggarapu@magilhub.com>
2025-03-03 15:17:59 +0530

    data base connectivity

---
+++ dbconnect.py
@@ -0,0 +1,13 @@
+import pandas as pd
+from sqlalchemy import create_engine
+
+# Read the Excel file (make sure openpyxl is installed for .xlsx files)
+df = pd.read_excel('/Users/yash/Downloads/Today/Splitted/input2.xlsx', engine='openpyxl')
+
+# Create a SQLAlchemy engine for a local MySQL database.
+# Replace 'username', 'password', 'localhost', and 'database_name' with your details.
+engine = create_engine('mysql+pymysql://root:Yaswanth123.@localhost/genai')
+
+# Import the DataFrame into MySQL. 
+# The 'if_exists' parameter can be set to 'fail', 'replace', or 'append'
+df.to_sql(name='reviews', con=engine, if_exists='append', index=False)
